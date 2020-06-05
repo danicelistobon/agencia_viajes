7e1f6c34164dba26cd94b448398791202eba6f11
danicelistobon <danielct-97@hotmail.com>
2020-06-04 19:16:47 -0500

    modified home controller adding async - await

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,23 +1,13 @@
-const Viajes = require('../models/Viajes');
-const Reviews = require('../models/Reviews');
+const Viaje = require('../models/Viajes');
+const Review = require('../models/Reviews');
 
-exports.consultasHome = (req, res) => {
-  const promises = [];
-  promises.push(Viajes.findAll({
-    limit: 3
-  }));
-  promises.push(Reviews.findAll({
-    limit: 3
-  }));
-
-  // pasar el promise y ejecutarlo
-  const resultado = Promise.all(promises);
-
-  resultado.then(resultado => res.render('index', {
+exports.consultasHome = async (req, res) => {
+  const viajes = await Viaje.findAll({ limit: 3 });
+  const reviews = await Review.findAll({ limit: 3 });
+  res.render('index', {
     clase: 'home',
     pagina: 'Próximos Viajes',
-    viajes: resultado[0],
-    reviews: resultado[1]
-  }))
-    .catch(error => console.log(error));
+    viajes: viajes,
+    reviews: reviews
+  });
 };
